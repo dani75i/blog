a0a1567b35ea2b321e15a53b32f413b0c5607ad1
Your Name <you@example.com>
2019-07-29 23:25:15 +0200

    remove crispy style

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
